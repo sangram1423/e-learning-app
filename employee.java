83041ad64bdb5e5388d345a2c3342b86e1340287
sangram1423 <technomsangru@gmail.com>
2023-03-29 23:52:20 +0530

    I also modified the same employee.java which is already modified by sangrutransient user with the same file, But I won't be knowing that

--- employee.java
+++ employee.java
@@ -4,4 +4,8 @@ sangrutransient user has changed the file employee.java
 
 Now sangram1423 is trying to change the the same file employee.java and pushing to remote repository.
 
+sangrutransient has modified the same file employee.java which 
+sangram1423 is also changing at the same time with the sangru1423 user.
+
+
 
