5fb702c5c58d6021021429f2f22bd8d749d5455c
sangram1423 <technomsangru@gmail.com>
2023-03-30 00:40:56 +0530

    I have fixed the BUG-122 priority one and we can use git stash apply to resume my BUG-121 work

--- user.java
+++ user.java
@@ -2,3 +2,9 @@ user registration happened
 
 I am commiting and pushing before pulling the latest which has made by sangram1423 user.
 check wether we will get error or not.
+
+
+
+I am fixed the BUG-122 and commited and pushed to remote repo because of more priority.
+
+Now we can do "git stash apply" to continue our work with BUG-121
